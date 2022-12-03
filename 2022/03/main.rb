cf67511cbb058de362ebaaa334aed476108374c8
Florian Kraft <schnuffifk@gmail.com>
2022-12-03 18:13:09 +0100

    runs rubocop
    thanks, incorrect subl config

--- 2022/03/main.rb
+++ 2022/03/main.rb
@@ -1,75 +1,77 @@
+# frozen_string_literal: true
+
 class Rucksack
-	attr_reader :input
+  attr_reader :input
 
-	def self.sort(input)
-		new(input:).sort
-	end
+  def self.sort(input)
+    new(input:).sort
+  end
 
-	def initialize(input:)
-		@input = input
-	end
+  def initialize(input:)
+    @input = input
+  end
 
-	def sort
-		input.reduce([]) do |memo, rucksack|
-			_head, left, right = rucksack.partition(/.{#{rucksack.size / 2}}/)
-			common = left.split('').intersection(right.split('')).pop
-			memo << { left:, right:, common:}
-		end
-	end
+  def sort
+    input.reduce([]) do |memo, rucksack|
+      _head, left, right = rucksack.partition(/.{#{rucksack.size / 2}}/)
+      common = left.split('').intersection(right.split('')).pop
+      memo << { left:, right:, common: }
+    end
+  end
 end
 
 class Priority
-	attr_reader :char
+  attr_reader :char
 
-	def self.calculate(char)
-		new(char:).calculate
-	end
+  def self.calculate(char)
+    new(char:).calculate
+  end
 
-	def initialize(char:)
-		@char = char
-	end
+  def initialize(char:)
+    @char = char
+  end
 
-	def calculate
-		position = char.upcase.ord - 'A'.ord + 1
+  def calculate
+    position = char.upcase.ord - 'A'.ord + 1
 
-		if char.downcase == char
-			position
-		else
-			position + 26
-		end
-	end
+    if char.downcase == char
+      position
+    else
+      position + 26
+    end
+  end
 end
 
 class BadgeFinder
-	attr_reader :group
+  attr_reader :group
 
-	def self.by_group(group)
-		new(group:).find
-	end
+  def self.by_group(group)
+    new(group:).find
+  end
 
-	def initialize(group:)
-		@group = group
-	end
+  def initialize(group:)
+    @group = group
+  end
 
-	def find
-		first, second, third = group.map { |rucksack| rucksack.split('') }
+  def find
+    first, second, third = group.map { |rucksack| rucksack.split('') }
 
-		first.intersection(second).intersection(third).pop
-	end
+    first.intersection(second).intersection(third).pop
+  end
 end
 
 rucksacks = File.read('./input').split("\n")
 
 part_one = Rucksack.sort(rucksacks).map do |rucksack|
-	rucksack[:common]
+  rucksack[:common]
 end.reduce(0) do |total, char|
-	total += Priority.calculate(char)
+  total += Priority.calculate(char)
 end
 
 part_two = rucksacks.each_slice(3).map do |group|
-	BadgeFinder.by_group group
+  BadgeFinder.by_group group
 end.reduce(0) do |total, char|
-	total += Priority.calculate(char)
+  total += Priority.calculate(char)
 end
 
 p part_two
--- 2022/03/spec/main_spec.rb
+++ 2022/03/spec/main_spec.rb
@@ -1,94 +1,96 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'pry'
 describe Rucksack do
-	describe '#sort' do
-		subject(:sorted) { described_class.sort(input) }
+  describe '#sort' do
+    subject(:sorted) { described_class.sort(input) }
 
-		let(:input) do
-			[
-				'vJrwpWtwJgWrhcsFMMfFFhFp',
-				'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-				'PmmdzqPrVvPwwTWBwg',
-				'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-				'ttgJtRGJQctTZtZT',
-				'CrZsJsPPZsGzwwsLwLmpwMDw'
-			]
-		end
+    let(:input) do
+      %w[
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+      ]
+    end
 
-		it { is_expected.to be_an Array }
+    it { is_expected.to be_an Array }
 
-		it do
-			is_expected.to eql [
-				{ left: 'vJrwpWtwJgWr', right: 'hcsFMMfFFhFp', common: 'p' },
-				{ left: 'jqHRNqRjqzjGDLGL', right: 'rsFMfFZSrLrFZsSL', common: 'L' },
-				{ left: 'PmmdzqPrV', right: 'vPwwTWBwg', common: 'P' },
-				{ left: 'wMqvLMZHhHMvwLH', right: 'jbvcjnnSBnvTQFn', common: 'v' },
-				{ left: 'ttgJtRGJ', right: 'QctTZtZT', common: 't' },
-				{ left: 'CrZsJsPPZsGz', right: 'wwsLwLmpwMDw', common: 's' },
-			]
-		end
-	end
+    it do
+      is_expected.to eql [
+        { left: 'vJrwpWtwJgWr', right: 'hcsFMMfFFhFp', common: 'p' },
+        { left: 'jqHRNqRjqzjGDLGL', right: 'rsFMfFZSrLrFZsSL', common: 'L' },
+        { left: 'PmmdzqPrV', right: 'vPwwTWBwg', common: 'P' },
+        { left: 'wMqvLMZHhHMvwLH', right: 'jbvcjnnSBnvTQFn', common: 'v' },
+        { left: 'ttgJtRGJ', right: 'QctTZtZT', common: 't' },
+        { left: 'CrZsJsPPZsGz', right: 'wwsLwLmpwMDw', common: 's' }
+      ]
+    end
+  end
 end
 
 describe Priority do
-	describe '#calcuate' do
-		subject(:priority) { described_class.calculate(character) }
+  describe '#calcuate' do
+    subject(:priority) { described_class.calculate(character) }
 
-		let(:character) { 'p' }
+    let(:character) { 'p' }
 
-		it { is_expected.to eql 16 }
+    it { is_expected.to eql 16 }
 
-		context 'when the character is "L"' do
-			let(:character) { 'L' }
-			it { is_expected.to eql 38 }
-		end
+    context 'when the character is "L"' do
+      let(:character) { 'L' }
+      it { is_expected.to eql 38 }
+    end
 
-		context 'when the character is "P"' do
-			let(:character) { 'P' }
-			it { is_expected.to eql 42 }
-		end
+    context 'when the character is "P"' do
+      let(:character) { 'P' }
+      it { is_expected.to eql 42 }
+    end
 
-		context 'when the character is "v"' do
-			let(:character) { 'v' }
-			it { is_expected.to eql 22 }
-		end
+    context 'when the character is "v"' do
+      let(:character) { 'v' }
+      it { is_expected.to eql 22 }
+    end
 
-		context 'when the character is "t"' do
-			let(:character) { 't' }
-			it { is_expected.to eql 20 }
-		end
+    context 'when the character is "t"' do
+      let(:character) { 't' }
+      it { is_expected.to eql 20 }
+    end
 
-		context 'when the character is "s"' do
-			let(:character) { 's' }
-			it { is_expected.to eql 19 }
-		end
-	end
+    context 'when the character is "s"' do
+      let(:character) { 's' }
+      it { is_expected.to eql 19 }
+    end
+  end
 end
 
 describe BadgeFinder do
-	describe '#by_group' do
-		subject(:badge) { described_class.by_group(group) }
+  describe '#by_group' do
+    subject(:badge) { described_class.by_group(group) }
 
-		let(:group) do
-			[
-				'vJrwpWtwJgWrhcsFMMfFFhFp',
-				'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-				'PmmdzqPrVvPwwTWBwg'
-			]
-		end
+    let(:group) do
+      %w[
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+      ]
+    end
 
-		it { is_expected.to eql 'r' }
+    it { is_expected.to eql 'r' }
 
-		context 'when it is the second group' do
-			let(:group) do
-				[
-					'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-					'ttgJtRGJQctTZtZT',
-					'CrZsJsPPZsGzwwsLwLmpwMDw'
-				]
-			end
+    context 'when it is the second group' do
+      let(:group) do
+        %w[
+          wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+          ttgJtRGJQctTZtZT
+          CrZsJsPPZsGzwwsLwLmpwMDw
+        ]
+      end
 
-			it { is_expected.to eql 'Z' }
-		end
-	end
+      it { is_expected.to eql 'Z' }
+    end
+  end
 end
