e78dd57ce7fbd5cfaea7ecc2b8c40f3907bc325e
Florian Kraft <schnuffifk@gmail.com>
2022-12-04 12:01:20 +0100

    adds solution for day 04 part 1

---
+++ 2022/day_04/README.md
@@ -0,0 +1,3 @@
+# Day04
+
+Solution for [Advent of Code 2022 Day 03](https://adventofcode.com/2022/day/4).
---
+++ 2022/day_04/lib/day04.ex
@@ -0,0 +1,64 @@
+defmodule Day04 do
+  @moduledoc """
+  Documentation for `Day04`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day04.contained?(["2-4", "6-8"])
+      false
+
+      iex> Day04.contained?(["6-6", "4-6"])
+      true
+
+      iex> Day04.contained?([])
+      false
+
+      iex> Day04.contained?(["5-6", "5-5"])
+      true
+
+  """
+
+  def contained?([]), do: false
+
+  def contained?([_, _] = shelves) do
+    shelves
+    |> Enum.map(fn str -> str |> String.split("-") end)
+    |> Enum.map(&to_int/1)
+    |> Enum.reject(&is_eol?/1)
+    |> Enum.map(fn [x, y] -> Range.new(x, y) end)
+    |> is_contained?
+  end
+
+  def contained?(_), do: false
+
+  def run do
+    {:ok, file} = File.read("./input")
+
+    file
+    |> :binary.bin_to_list()
+    |> to_string
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, ","))
+  end
+
+  defp to_int([""]), do: :eol
+
+  defp to_int(strings) do
+    strings |> Enum.map(&String.to_integer/1)
+  end
+
+  defp is_eol?(:eol), do: true
+  defp is_eol?(_), do: false
+
+  defp is_contained?([shelf_one, shelf_two] = shelves) do
+    shelves
+    |> Enum.map(&MapSet.new/1)
+    |> then(fn [x, y] = args ->
+      MapSet.subset?(x, y) || MapSet.subset?(y, x)
+    end)
+  end
+end
