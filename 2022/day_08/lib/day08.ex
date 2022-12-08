af88105b3b896c32af0912bf4f00e1a7b72cd624
Florian Kraft <schnuffifk@gmail.com>
2022-12-08 22:57:00 +0100

    fuck me i guess

--- 2022/day_08/lib/day08.ex
+++ 2022/day_08/lib/day08.ex
@@ -4,65 +4,141 @@ defmodule Day08 do
   """
 
   @doc """
-  Finds the message after a message marker
+  counts the visible trees in a given list of lists
 
-  ## Examples
+  ### Examples
 
-    iex> Day08.find_start_of_message("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-    19
-    iex> Day08.find_start_of_message("bvwbjplbgvbhsrlpgdmjqwftvncz")
-    23
-    iex> Day08.find_start_of_message("nppdvjthqldpwncqszvftbrmjlhg")
-    23
-    iex> Day08.find_start_of_message("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-    29
-    iex> Day08.find_start_of_message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
-    26
+      iex> Day08.count_visible([
+      iex>   [3,0,3,7,3],
+      iex>   [2,5,5,1,2],
+      iex>   [6,5,3,3,2],
+      iex>   [3,3,5,4,9],
+      iex>   [3,5,3,9,0]
+      iex> ])
+      21
 
-  """
-  def find_start_of_message(input) do
-    input |> String.split("") |> Enum.reject(fn str -> str === "" end) |> find_sequence(14)
-  end
-
-  @doc """
-  Finds the start of a message by looking for the position at which the message marker is complete
-
-  ## Examples
-
-      iex> Day08.find_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-      7
-
-      iex> Day08.find_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz")
+      iex> Day08.count_visible([[1,1,1,2,3]])
       5
 
-      iex> Day08.find_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg")
+      iex> Day08.count_visible([
+      iex>   [1,1,1],
+      iex>   [1,1,1],
+      iex> ])
       6
 
-      iex> Day08.find_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-      10
+      iex> Day08.count_visible([
+      iex>   [1,1,1],
+      iex>   [1,6,1],
+      iex>   [1,1,1],
+      iex> ])
+      9
 
-      iex> Day08.find_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+      iex> Day08.count_visible([
+      iex>   [1,8,1],
+      iex>   [6,6,8],
+      iex>   [1,7,1],
+      iex>   [1,1,1],
+      iex> ])
       11
 
+      iex> Day08.count_visible([
+      iex>   [1,8,1,1],
+      iex>   [6,6,8,1],
+      iex>   [1,7,1,1],
+      iex>   [1,1,1,1],
+      iex> ])
+      14
+
+      iex> Day08.count_visible([
+      iex>   [1,1,1,1,1],
+      iex>   [1,6,1,1,1],
+      iex>   [1,1,3,2,1],
+      iex>   [1,1,2,2,2],
+      iex>   [1,1,1,2,1],
+      iex> ])
+      20
+
+      iex> Day08.count_visible([
+      iex>   [3,0,3,7,3],
+      iex>   [2,5,1,1,2],
+      iex>   [6,5,3,3,2],
+      iex>   [3,3,5,4,9],
+      iex>   [3,5,3,9,0]
+      iex> ])
+      20
+
+
   """
-  def find_start_of_packet(input) do
-    input
-    |> String.split("")
-    |> Enum.reject(fn str -> str == "" end)
-    |> find_sequence()
+  def count_visible(trees) do
+    trees
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {tree_line, row}, outer ->
+      cond do
+        # top row, done
+        row == 0 ->
+          outer + length(tree_line)
+
+        # bottom row, done
+        row == length(trees) - 1 ->
+          outer + length(tree_line)
+
+        true ->
+          tree_line
+          |> Enum.with_index()
+          |> Enum.reduce(outer, fn {height, col}, inner ->
+            cond do
+              # start or end of tree line
+              col == 0 || col == length(tree_line) - 1 ->
+                inner + 1
+
+              # visible from the top
+              height |> visible?(:top, row, col, tree_line, trees) ->
+                # {{row, col}, height, :top} |> IO.inspect()
+                inner + 1
+
+              # visible from the right
+              height |> visible?(:right, row, col, tree_line, trees) ->
+                # {{row, col}, height, :right} |> IO.inspect()
+                inner + 1
+
+              # visible from the bottom
+              height |> visible?(:bottom, row, col, tree_line, trees) ->
+                # {{row, col}, height, :bottom} |> IO.inspect()
+                inner + 1
+
+              # visible from the left
+              height |> visible?(:left, row, col, tree_line, trees) ->
+                # {{row, col}, height, :left} |> IO.inspect()
+                inner + 1
+
+              true ->
+                # {{row, col}, height, :none} |> IO.inspect()
+                inner
+            end
+          end)
+      end
+    end)
   end
 
-  defp find_sequence(list \\ [], len \\ 4, idx \\ 0)
-
-  defp find_sequence(list, len, idx) do
-    if list |> Enum.slice(idx, len) |> all_different? do
-      idx + len
-    else
-      find_sequence(list, len, idx + 1)
-    end
+  defp column(trees, idx) do
+    trees |> Enum.map(&Enum.at(&1, idx))
   end
 
-  defp all_different?(list) do
-    list |> MapSet.new() |> MapSet.size() == length(list)
+  defp visible?(height, :top, _row, col, _line, trees) do
+    height > trees |> column(col) |> Enum.slice(0..col) |> Enum.max()
   end
+
+  defp visible?(height, :right, _row, col, line, _trees) do
+    height > Enum.slice(line, (col + 1)..(length(line) - 1)) |> Enum.max()
+  end
+
+  defp visible?(height, :bottom, row, col, _line, trees) do
+    height > trees |> column(col) |> Enum.slice((row + 1)..(length(trees) - 1)) |> Enum.max()
+  end
+
+  defp visible?(height, :left, _row, col, line, _trees) do
+    height > Enum.slice(line, 0..(col - 1)) |> Enum.max()
+  end
+
+  defp visible?(_, _, _, _, _, _), do: false
 end
--- 2022/day_08/mix.exs
+++ 2022/day_08/mix.exs
@@ -5,7 +5,7 @@ defmodule Day08.MixProject do
     [
       app: :day_08,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
